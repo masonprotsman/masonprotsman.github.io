fc19d61f129a5fdc66d4ce64f693daa5c7cb2a77
masonprotsman <masonprotsman@gmail.com>
2026-01-06 10:28:03 -0500

    fixed vite config for dev and prod

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/3D-portfolio',
+  base: process.env.NODE_ENV === 'production' ? '/3D-portfolio' : '/',
 })
